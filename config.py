05f8ef0a511b14f8bd1a57062667e194fb3d4b91
howljenkins2003-cpu <howljenkins2003@gmail.com>
2026-05-31 20:03:33 +0545

    v0: project skeleton and dependencies

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+MODEL = "llama-3.3-70b-versatile"
+WAKE_WORD = "jarvis"
+
+TTS_LANGUAGE = "en"
+TTS_SLOW = False
