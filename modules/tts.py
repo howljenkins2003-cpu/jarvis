e9c0845e6c3fde9eae846bb4006094ba9fedb50a
howljenkins2003-cpu <howljenkins2003@gmail.com>
2026-06-02 18:49:51 +0545

    v1.5: edge-tts voice update (Ryan)

--- modules/tts.py
+++ modules/tts.py
@@ -1,10 +1,16 @@
-from gtts import gTTS
+import edge_tts
+import asyncio
 import subprocess
 import os
 
-def speak(text):
-    tts = gTTS(text=text, lang="en", slow=False)
-    tts.save("response.mp3")
+VOICE = "en-GB-RyanNeural" # ryan voice.
+
+async def _speak_async(text):
+    communicate = edge_tts.Communicate(text, VOICE)
+    await communicate.save("response.mp3")
     subprocess.run(["mpg123", "-q", "response.mp3"])
     os.remove("response.mp3")
+
+def speak(text):
+    asyncio.run(_speak_async(text))
 
